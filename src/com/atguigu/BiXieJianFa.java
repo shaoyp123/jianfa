265e9cebb80b9bc0ccf733c9995e8e6875a932c3
shaoyp123 <yp_shao@sina.com>
2016-12-10 18:10:50 +0800

    update
    aaaa

--- src/com/atguigu/BiXieJianFa.java
+++ src/com/atguigu/BiXieJianFa.java
@@ -5,5 +5,7 @@ public class BiXieJianFa {
 	public static void main(String[] args) {
 		System.out.println("test bixiejianfa ...");
 		System.out.println("add new content ...");
+		System.out.println("add new content ...");
+		
 	}
 }
